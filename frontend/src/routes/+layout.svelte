e7895c34cbb8a557117d34b44ac4570dc7c759e1
KevsRepos <kevinsheardg@gmail.com>
2026-03-10 01:45:49 +0100

    Reinstalling Svelte (using v5)

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<header class="border-b shadow-md">
+	<a href="/" class="falcon-poster-bold text-3xl pl-2">Workers</a>
+</header>
+
+{@render children()}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,5 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-});
+export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
