58eea2308afa551f2c05d45c27021c898d2deb23
KevsRepos <kevinsheardg@gmail.com>
2026-04-19 00:58:08 +0200

    refactor: remove unused assertions and commented code in product request DTOs

--- backend/src/Modules/Pim/Product/Dto/CreateProductRequestDto.php
+++ backend/src/Modules/Pim/Product/Dto/CreateProductRequestDto.php
@@ -14,11 +14,7 @@ class CreateProductRequestDto
     #[Assert\GreaterThanOrEqual(0)]
     public ?int $salesPrice;
 
-    #[Assert\NotBlank]
     public ?string $depositId;
-
-    // #[Assert\Type('bool')]
-    // public bool $bundable = false;
 
     #[Assert\Type('bool')]
     public bool $sellable = false;
