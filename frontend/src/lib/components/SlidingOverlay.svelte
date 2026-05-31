8c23796abb2781daf7ce88d168d8686fee5f902c
KevsRepos <kevinsheardg@gmail.com>
2026-05-31 22:10:09 +0200

    feat: add SlidingOverlay component for managing time tracking entries

---
+++ frontend/src/lib/components/SlidingOverlay.svelte
@@ -0,0 +1,20 @@
+<script>
+import { X } from "@lucide/svelte";
+import { slide } from "svelte/transition";
+
+let { close, title, body } = $props();
+</script>
+
+<div transition:slide class="fixed top-full -translate-y-full inset-0 z-50 bg-surface-100 text-surface-950 flex flex-col w-full h-4/6 rounded-2xl">
+    <div class="flex justify-between p-4 border-b">
+        <div class="font-bold">{@render title()}</div>
+
+        <button onclick={close}>
+            <X />
+        </button>
+    </div>
+
+    <div class="p-2">
+        {@render body()}
+    </div>
+</div>
--- frontend/src/routes/time-tracking/+page.svelte
+++ frontend/src/routes/time-tracking/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { invalidateAll } from "$app/navigation";
 import PageHeadline from "$lib/components/PageHeadline.svelte";
+import SlidingOverlay from "$lib/components/SlidingOverlay.svelte";
 import TopNavigation from "$lib/components/TopNavigation.svelte";
 import { fetchApi } from "$lib/fetchApi";
 import { Bell, CalendarClock, ChevronDown, ChevronRight, LayoutList, Pen, } from "@lucide/svelte";
@@ -8,16 +9,14 @@ import { Navigation } from "@skeletonlabs/skeleton-svelte";
 
 const { data } = $props();
 
-$inspect(data);
-
 let day = $state(new Date().getDate());
 let start = $state('');
 let breakDuration = $state(0);
 let end = $state('');
 
-let entryInputs: Record<number, { start: string; breakDuration: number; end: string }> = $state({});
-
 let editingEntryId: string | null = $state(null);
+
+
 
 const ensureTimeSheet = async (): Promise<string | null> => {
     let timeSheetId = data.timeSheet?.id;
@@ -46,6 +45,12 @@ const saveEntry = async (entryDay: number, entryStart: string, entryBreakDuratio
         });
 
         await invalidateAll();
+
+        newEntry = false;
+        dayIndex = null;
+        start = '';
+        breakDuration = 0;
+        end = '';
     } catch (e) {
         console.error(e);
     }
@@ -53,29 +58,56 @@ const saveEntry = async (entryDay: number, entryStart: string, entryBreakDuratio
 
 const saveToday = () => saveEntry(day, start, breakDuration, end);
 
-const saveForDay = (dayIndex: number) => {
-    const input = entryInputs[dayIndex];
-    if (!input) return;
-    saveEntry(dayIndex, input.start, input.breakDuration, input.end);
+const saveForDay = (dayIndex: number | null) => {    
+    if (dayIndex === null) return;
+    
+    saveEntry(dayIndex, start, breakDuration, end);
 }
 
-const updateForDay = async (dayIndex: number, entryId: string) => {
-    const input = entryInputs[dayIndex];
-    if (!input) return;
+const updateForDay = async (dayIndex: number, entry: { id: string; start: string; breakDuration: number; end: string }) => {
     try {
-        await fetchApi(`time-sheets/entries/${entryId}`, 'PUT', {
+
+        console.log(dayIndex, entry);
+
+        await fetchApi(`time-sheets/entries/${entry.id}`, 'PUT', {
             day: dayIndex,
-            start: input.start,
-            breakDuration: input.breakDuration,
-            end: input.end
+            start: entry.start,
+            breakDuration: entry.breakDuration,
+            end: entry.end
         });
+
+
+
         editingEntryId = null;
+        start = '';
+        breakDuration = 0;
+        end = '';
+
         await invalidateAll();
     } catch (e) {
         console.error(e);
     }
 }
 
+const editEntry = (entryId: string) => {
+    const entry = data.timeSheet.entries.find((e: any) => e ? e.id === entryId : false);
+
+    if (entry) {
+        start = entry.start;
+        breakDuration = entry.breakDuration;
+        end = entry.end;
+    }
+
+    editingEntryId = entryId;
+}
+
+let newEntry = $state(false);
+
+let dayIndex: number | null = $state(null);
+
+const addNew = () => {
+    newEntry = true;
+}
 </script>
 
 <TopNavigation>
@@ -97,79 +129,128 @@ const updateForDay = async (dayIndex: number, entryId: string) => {
 
 <main class="lg:max-w-200 mx-auto">
     {#if !data.timeSheet?.entries?.[new Date().getDate() - 1]}
-        <div class="flex flex-col gap-2 border-b px-2 pb-4 mb-4">
-            <div>Eintrag für Heute:</div>
+        <button class="btn preset-filled flex m-auto" onclick={addNew}>
+            Für heute hinzufügen
+            <ChevronRight />
+        </button>
+    {/if}
 
-            <label>
-                <span class="label-text">Beginn</span>
-                <input class="input" step="1800" type="time" placeholder="Beginn (8:00)" bind:value={start} />
-            </label>
-            <label>
-                <span class="label-text">Pause (Minuten)</span>
-                <input class="input" type="number" placeholder="Pause (60 Min)" bind:value={breakDuration} />
-            </label>
-            <label>
-                <span class="label-text">Ende</span>
-                <input class="input" step="1800" type="time" placeholder="Ende (17:00)" bind:value={end} />
-            </label>
-            <button onclick={saveToday} type="button" class="btn preset-filled">
-                Tag hinzufügen
-                <ChevronDown />
-            </button>
-        </div>
+    {#if newEntry}
+        <SlidingOverlay close={() => newEntry = false}>
+             {#snippet title()}
+                Neuer Eintrag Heute
+             {/snippet}
+
+             {#snippet body()}
+                <div class="flex flex-col gap-2 border-b pb-4 mb-4">
+                    <label>
+                        <span class="label-text">Beginn</span>
+                        <input class="input bg-surface-50" step="1800" type="time" placeholder="Beginn (8:00)" bind:value={start} />
+                    </label>
+                    <label>
+                        <span class="label-text">Pause (Minuten)</span>
+                        <input class="input bg-surface-50" type="number" placeholder="Pause (60 Min)" bind:value={breakDuration} />
+                    </label>
+                    <label>
+                        <span class="label-text">Ende</span>
+                        <input class="input bg-surface-50" step="1800" type="time" placeholder="Ende (17:00)" bind:value={end} />
+                    </label>
+                    <button onclick={saveToday} type="button" class="btn preset-filled">
+                        Tag hinzufügen
+                        <ChevronDown />
+                    </button>
+                </div>
+             {/snippet}
+        </SlidingOverlay>
     {/if}
 
     {#if data.timeSheet}
         <div class="my-2 flex justify-center">
             <div class="badge preset-filled-surface-500">Gesamtstunden: {data.timeSheet.totalHours} Std</div>
         </div>
+
         <div class="flex flex-col mt-4 w-full">
             {#each data.timeSheet.entries as entry, index}
                 {#if entry}
-                    {#if editingEntryId === entry.id}
-                        <div style="grid-template-columns: 6% auto;" class="grid gap-2 border-b w-full h- bg-surface-100-900 px-2">
-                            <div class="border-r pr-2 font-bold">{index + 1}</div>
-                            <div class="flex flex-col gap-2 py-2">
-                                <div class="flex gap-2">
-                                    <input placeholder="Beginn" step="1800" type="time" style="display:inline-block; width:33.33%" oninput={(e) => { entryInputs[index + 1] = { ...entryInputs[index + 1] ?? { start: '', breakDuration: 0, end: '' }, start: e.currentTarget.value }; }} />
-                                    <input placeholder="Pause" type="number" style="display:inline-block; width:33.33%" oninput={(e) => { entryInputs[index + 1] = { ...entryInputs[index + 1] ?? { start: '', breakDuration: 0, end: '' }, breakDuration: Number(e.currentTarget.value) }; }} />
-                                    <input placeholder="Ende" step="1800" type="time" style="display:inline-block; width:33.33%" oninput={(e) => { entryInputs[index + 1] = { ...entryInputs[index + 1] ?? { start: '', breakDuration: 0, end: '' }, end: e.currentTarget.value }; }} />
-                                </div>
-                                <button class="btn btn-sm preset-filled mb-2" onclick={() => updateForDay(index + 1, entry.id)}>
-                                    Hinzufügen
-                                    <ChevronRight size="16"/>
+                    <div style="grid-template-columns: 6% auto;" class="grid gap-2 border-b px-2">
+                        <div class="border-r pr-2 font-bold">{entry.day}</div>
+                        <div class="flex justify-between w-full">
+                            <div>{entry.start} - {entry.end} (Pause: {entry.breakDuration} Min)</div>
+                            <div>
+                                {entry.totalHours} Std
+                                <button onclick={() => editEntry(entry.id)}>
+                                    <Pen size="16" />
                                 </button>
                             </div>
                         </div>
-                    {:else}
-                        <div style="grid-template-columns: 6% auto;" class="grid gap-2 border-b px-2">
-                            <div class="border-r pr-2 font-bold">{entry.day}</div>
-                            <div class="flex justify-between w-full">
-                                <div>{entry.start} - {entry.end} (Pause: {entry.breakDuration} Min)</div>
-                                <div>
-                                    {entry.totalHours} Std
-                                    <button onclick={() => editingEntryId = entry.id}>
-                                        <Pen size="16" />
+                    </div>
+
+                    {#if editingEntryId === entry.id}
+                        <SlidingOverlay close={() => editingEntryId = null}>
+                            {#snippet title()}
+                                Eintrag für Tag {entry.day} bearbeiten
+                            {/snippet}
+
+                            {#snippet body()}
+                                <div class="flex flex-col gap-2 border-b pb-4 mb-4">
+                                    <label>
+                                        <span class="label-text">Beginn</span>
+                                        <input class="input bg-surface-50" step="1800" type="time" placeholder="Beginn (8:00)" bind:value={start}/>
+                                    </label>
+                                    <label>
+                                        <span class="label-text">Pause (Minuten)</span>
+                                        <input class="input bg-surface-50" type="number" placeholder="Pause (60 Min)" bind:value={breakDuration}/>
+                                    </label>
+                                    <label>
+                                        <span class="label-text">Ende</span>
+                                        <input class="input bg-surface-50" step="1800" type="time" placeholder="Ende (17:30)" bind:value={end}/>
+                                    </label>
+                                    <button onclick={() => updateForDay(index + 1, { id: entry.id, start, breakDuration, end })} type="button" class="btn preset-filled">
+                                        Speichern
+                                        <ChevronDown />
                                     </button>
                                 </div>
-                            </div>
-                        </div>
+                            {/snippet}
+                        </SlidingOverlay>
                     {/if}
                 {:else if index < new Date().getDate() - 1}
-                    <div style="grid-template-columns: 6% auto;" class="grid gap-2 border-b w-full h- bg-surface-100-900 px-2">
+                    <div style="grid-template-columns: 6% auto;" class="grid gap-2 border-b w-full bg-surface-100-900 px-2">
                         <div class="border-r pr-2 font-bold">{index + 1}</div>
-                        <div class="flex flex-col gap-2 py-2">
-                            <div class="flex gap-2">
-                                <input placeholder="Beginn" step="1800" type="time" style="display:inline-block; width:33.33%" oninput={(e) => { entryInputs[index + 1] = { ...entryInputs[index + 1] ?? { start: '', breakDuration: 0, end: '' }, start: e.currentTarget.value }; }} />
-                                <input placeholder="Pause" type="number" style="display:inline-block; width:33.33%" oninput={(e) => { entryInputs[index + 1] = { ...entryInputs[index + 1] ?? { start: '', breakDuration: 0, end: '' }, breakDuration: Number(e.currentTarget.value) }; }} />
-                                <input placeholder="Ende" step="1800" type="time" style="display:inline-block; width:33.33%" oninput={(e) => { entryInputs[index + 1] = { ...entryInputs[index + 1] ?? { start: '', breakDuration: 0, end: '' }, end: e.currentTarget.value }; }} />
-                            </div>
-                            <button class="btn btn-sm preset-filled mb-2" onclick={() => saveForDay(index + 1)}>
-                                Hinzufügen
-                                <ChevronRight size="16"/>
-                            </button>
-                        </div>
+
+                        <button class="btn btn-sm preset-filled my-2" onclick={() => dayIndex = index + 1}>
+                            Hinzufügen
+                            <ChevronRight size="16"/>
+                        </button>
                     </div>
+
+                    {#if dayIndex !== null}
+                        <SlidingOverlay close={() => dayIndex = null}>
+                            {#snippet title()}
+                                Neuer Eintrag für Tag {dayIndex}
+                            {/snippet}
+
+                            {#snippet body()}
+                                <div class="flex flex-col gap-2 border-b pb-4 mb-4">
+                                    <label>
+                                        <span class="label-text">Beginn</span>
+                                        <input class="input bg-surface-50" step="1800" type="time" placeholder="Beginn (8:00)" bind:value={start}/>
+                                    </label>
+                                    <label>
+                                        <span class="label-text">Pause (Minuten)</span>
+                                        <input class="input bg-surface-50" type="number" placeholder="Pause (60 Min)" bind:value={breakDuration}/>
+                                    </label>
+                                    <label>
+                                        <span class="label-text">Ende</span>
+                                        <input class="input bg-surface-50" step="1800" type="time" placeholder="Ende (17:00)" bind:value={end}/>
+                                    </label>
+                                    <button onclick={() => saveForDay(dayIndex)} type="button" class="btn preset-filled">
+                                        Tag hinzufügen
+                                        <ChevronDown />
+                                    </button>
+                                </div>
+                            {/snippet}
+                        </SlidingOverlay>
+                    {/if}
                 {/if}
             {/each}
         </div>
