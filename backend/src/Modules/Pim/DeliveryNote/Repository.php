e94499d4a197100ba1aaf25a79a4439adf73b8e1
KevsRepos <kevinsheardg@gmail.com>
2026-06-13 09:19:33 +0200

    fix: change delivery note status sorting order from ASC to DESC

--- backend/src/Modules/Pim/DeliveryNote/Repository.php
+++ backend/src/Modules/Pim/DeliveryNote/Repository.php
@@ -31,7 +31,7 @@ class Repository extends ServiceEntityRepository
      */
     public function findByStatus(DeliveryNoteStatus $status): array
     {
-        return $this->findBy(['status' => $status], ['deliveryDate' => 'ASC']);
+        return $this->findBy(['status' => $status], ['deliveryDate' => 'DESC']);
     }
 
     public function saveDeliveryNote(DeliveryNote $entity, bool $flush = false): string
